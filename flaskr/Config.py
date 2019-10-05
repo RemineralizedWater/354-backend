03f640add852ce59e285824d035f433125e377a5
Guillaume Rochefort-Mathieu <guillaume.rochefort.mathieu@gmail.com>
2019-10-05 09:07:10 -0400

    Added Email Functionality
    Added a email function that sends emails. Note that the email must be authorized on mailgun to
    receive emails. This will change later on.

--- flaskr/Config.py
+++ flaskr/Config.py
@@ -3,4 +3,7 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'DEV'
     DATABASE_URL = os.environ.get('DATABASE_URL') or ''
-    
+    SMTP_HOST = 'smtp.mailgun.org' # Placeholder
+    SMTP_PORT = 587 # Placeholder
+    SMTP_USERNAME = 'postmaster@sandboxf55aa089a27a4ed686e941fee0ddd632.mailgun.org' # Placeholder
+    SMTP_PASSWORD = 'f85b0f946e0c2262f9f6be0cfa99bd9d-b892f62e-6a855db0' # Placeholder
